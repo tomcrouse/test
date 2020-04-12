f13c60c45329e30cbe3d1d339e5c5844e0e2857c
Tony Kark <tonywebdevkark@gmail.com>
2020-04-12 21:12:18 +0300

    build

---
+++ precache-manifest.e4f11ddd090f3b29a7e9f011521f8fac.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a63408366be7ca01e90f38688652d172",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2473d67d6449d74bfcea",
+    "url": "/static/css/main.81aaaf67.chunk.css"
+  },
+  {
+    "revision": "decf706c6b6dee511331",
+    "url": "/static/js/2.68827a74.chunk.js"
+  },
+  {
+    "revision": "b67bc97fd0dd67004287af4028afc1b1",
+    "url": "/static/js/2.68827a74.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2473d67d6449d74bfcea",
+    "url": "/static/js/main.c0bcee21.chunk.js"
+  },
+  {
+    "revision": "702554416a81a1e3a894",
+    "url": "/static/js/runtime-main.784c586e.js"
+  }
+]);
